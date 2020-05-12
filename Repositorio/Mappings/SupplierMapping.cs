8247f00880a1a4ee05be64e5af022bdb30e635f1
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-11 21:27:02 -0300

    SupplierMapping

---
+++ Repositorio/Mappings/SupplierMapping.cs
@@ -0,0 +1,27 @@
+using BludataTest.Enums;
+using BludataTest.Models;
+using BludataTest.ValueObject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BludataTest.Repositorio
+{
+    public class SupplierMapping 
+        : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.Property(x => x.Document).HasConversion(y => y.ToString(), v => new Document(v, GetDocumentType(v)));
+        }
+
+        private EDocumentType GetDocumentType(string v)
+        {
+            if (v.Length == 11)
+            {
+                return EDocumentType.CPF;
+            }
+            return EDocumentType.CNPJ;
+        }
+    }
+    
+}
