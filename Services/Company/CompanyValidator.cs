122ce0ad7513dcaf747f5da8d2745d4161cb96bc
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-11 21:33:01 -0300

    Escopo da CompanyValidator

--- Services/Company/CompanyValidator.cs
+++ Services/Company/CompanyValidator.cs
@@ -1,8 +1,27 @@
+using BludataTest.Models;
+
 namespace BludataTest.Services
 {
     public class CompanyValidator
-{
+    {
+        public bool isValid(Company company)
+        {
+            if(company.TradingName.Length <2)
+            {
+                return false;
+            }
+            if(isCNPJValid(company.Document.ToString()))
+            {
+                return false;
+            }
+            //validar coisitchas here
+            return false;
+        }
 
+        private bool isCNPJValid(string cnpj)
+        {
+            return false;
+        }
     }
 
 }
