3f7604045ce963249211928d96dd5f7724390632
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-13 11:17:59 -0300

    Validações Empresa

--- Services/Company/CompanyValidator.cs
+++ Services/Company/CompanyValidator.cs
@@ -6,22 +6,50 @@ namespace BludataTest.Services
     {
         public bool isValid(Company company)
         {
-            if(company.TradingName.Length <2)
-            {
-                return false;
-            }
-            if(isCNPJValid(company.Document.ToString()))
-            {
-                return false;
-            }
-            //validar coisitchas here
-            return false;
+            if(company.TradingName == null || company.TradingName.Length < 2)
+              return false;
+            if(company.UF == null || company.UF.Length > 2)
+              return false;
+            if(!isCNPJValid(company.Document.ToString()))
+              return false ;
+            return true;
         }
 
         private bool isCNPJValid(string cnpj)
         {
-            return false;
+            //ValidadorCNPJ - Macoratti
+			int[] multiplicador1 = new int[12] {5,4,3,2,9,8,7,6,5,4,3,2};
+			int[] multiplicador2 = new int[13] {6,5,4,3,2,9,8,7,6,5,4,3,2};
+			int soma;
+			int resto;
+			string digito;
+			string tempCnpj;
+			cnpj = cnpj.Trim();
+			cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+			if (cnpj.Length != 14)
+			   return false;
+			   tempCnpj = cnpj.Substring(0, 12);
+			   soma = 0;
+			for(int i=0; i<12; i++)
+			   soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+			   resto = (soma % 11);
+			if ( resto < 2)
+			   resto = 0;
+			else
+			   resto = 11 - resto;
+			    digito = resto.ToString();
+			    tempCnpj = tempCnpj + digito;
+			    soma = 0;
+			for (int i = 0; i < 13; i++)
+			   soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+			   resto = (soma % 11);
+			if (resto < 2)
+			    resto = 0;
+			else
+			   resto = 11 - resto;
+			   digito = digito + resto.ToString();
+
+			return cnpj.EndsWith(digito);
         }
     }
-
 }
