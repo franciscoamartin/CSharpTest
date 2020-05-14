d52ba0a43a94735035abcec007232872a77ce27f
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-14 19:40:30 -0300

    Adição construtores

--- Application/Models/Company.cs
+++ Application/Models/Company.cs
@@ -5,9 +5,19 @@ namespace BludataTest.Models
 {
     public class Company
     {
+        public Company(string uf, string tradingName, Document document)
+        {
+            UF = uf;
+            TradingName = tradingName;
+            Document = document;
+        }
+
         public Guid Id { get; protected set; }
         public string UF { get; set; }
         public string TradingName { get; set; }
         public Document Document { get; protected set; }
+
+
+
     }
 }
--- Application/Models/Supplier.cs
+++ Application/Models/Supplier.cs
@@ -6,6 +6,16 @@ namespace BludataTest.Models
 {
     public class Supplier
     {
+        public Supplier(string name, Company company, Document document, DateTime registerTime, DateTime birthDate, string telephone)
+        {
+            Name = name;
+            Company = company;
+            Document = document;
+            RegisterTime = registerTime;
+            BirthDate = birthDate;
+            Telephone = telephone;
+        }
+
         public Guid Id { get; protected set; }
         public string Name { get; set; }
         public Company Company { get; protected set; }
