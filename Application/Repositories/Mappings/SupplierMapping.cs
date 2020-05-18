d352a7423acc22b0eba5edd10f69460f75dbd5d6
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-18 20:30:40 -0300

    Chave Estrangeria Empresa

--- Application/Repositories/Mappings/SupplierMapping.cs
+++ Application/Repositories/Mappings/SupplierMapping.cs
@@ -11,11 +11,29 @@ namespace BludataTest.Repositories
     {
         public void Configure(EntityTypeBuilder<Supplier> builder)
         {
+            builder.HasOne(x => x.Company).WithMany().HasForeignKey(x => x.CompanyId).IsRequired();
             builder.Property(x => x.Document).HasConversion(y => y.ToString(), v => new Document(v, GetDocumentType(v)));
             builder.Property(x => x.BirthDate).IsRequired();
             builder.Property(x => x.RegisterTime).IsRequired();
             builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
-            //builder.HasMany(x => x.Telephone);
+            builder.Property(x => x.Telephones).HasConversion(t => GetTelephonesString(t), telephoneString => GetTelephoneArray(telephoneString));
+        }
+
+        private string GetTelephonesString(string[] telephones)
+        {
+            var resultString = "";
+            foreach (var telephone in telephones)
+            {
+                resultString += telephone + ",";                                
+            }
+            resultString = resultString.Remove(resultString.Length -1);
+            return resultString;
+        }
+
+        private string[] GetTelephoneArray(string telephones)
+        {
+            var resultString = telephones.Split(',');
+            return resultString;
         }
 
         private EDocumentType GetDocumentType(string v)
