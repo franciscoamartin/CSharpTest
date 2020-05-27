87b14038ff100ec612501de7b1d73016daaf656e
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-27 10:26:40 -0300

    Alterações relacionadas á erros internos

--- backend/Application/Startup.cs
+++ backend/Application/Startup.cs
@@ -15,46 +15,38 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BludataTest
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace BludataTest {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<BludataTestDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddTransient<ICompanyRepository, CompanyRepository>();
-            services.AddTransient<ICompanyService, CompanyService>();
-            services.AddTransient<ISupplierRepository, SupplierRepository>();
-            services.AddTransient<ISupplierService, SupplierService>();
-            services.AddMvc();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<BludataTestDbContext> (options =>
+                options.UseSqlServer ("Server=localhost\\SQLEXPRESS;User Id=sa;Password=sa;Database=BludataTest;MultipleActiveResultSets=true"));
+            services.AddTransient<ICompanyRepository, CompanyRepository> ();
+            services.AddTransient<ICompanyService, CompanyService> ();
+            services.AddTransient<ISupplierRepository, SupplierRepository> ();
+            services.AddTransient<ISupplierService, SupplierService> ();
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
+            app.UseCors (option => option.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ());
 
-            app.UseHttpsRedirection();
+            app.UseRouting ();
 
-            app.UseRouting();
+            app.UseAuthorization ();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
