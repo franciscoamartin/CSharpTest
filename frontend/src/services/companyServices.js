ec2dcd979e67ec3d9c5bc09dde4a7dc6921fb581
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-27 10:10:32 -0300

    Criação "CompanyService"

---
+++ frontend/src/services/companyServices.js
@@ -0,0 +1,23 @@
+import api from './api';
+
+export async function getCompany(id) {
+  const response = await api.get(`/company/${id}`);
+  return response.data;
+}
+export async function getAllCompanies() {
+  const response = await api.get(`/company`);
+  debugger;
+  return response.data;
+}
+export async function updateCompany(company) {
+  const response = await api.put(`/company/${company.id}`, company);
+  return response.data;
+}
+export async function deleteCompany(id) {
+  const response = await api.delete(`/company/${id}`);
+  return response.data;
+}
+export async function createCompany(company) {
+  const response = await api.post(`/company`, company);
+  return response.data;
+}
