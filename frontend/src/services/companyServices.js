e3584de820cc9298312821904c50b76738a1da8b
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-29 22:10:00 -0300

    Organização código

--- frontend/src/services/companyServices.js
+++ frontend/src/services/companyServices.js
@@ -1,12 +1,15 @@
 import api from './api';
 
+export async function createCompany(company) {
+  const response = await api.post(`/company`, company);
+  return response.data;
+}
 export async function getCompany(id) {
   const response = await api.get(`/company/${id}`);
   return response.data;
 }
 export async function getAllCompanies() {
   const response = await api.get(`/company`);
-  debugger;
   return response.data;
 }
 export async function updateCompany(company) {
@@ -15,9 +18,5 @@ export async function updateCompany(company) {
 }
 export async function deleteCompany(id) {
   const response = await api.delete(`/company/${id}`);
-  return response.data;
-}
-export async function createCompany(company) {
-  const response = await api.post(`/company`, company);
   return response.data;
 }
--- frontend/src/services/supplierServices.js
+++ frontend/src/services/supplierServices.js
@@ -1,12 +1,38 @@
 import api from './api';
 
+export async function createSupplier(supplier) {
+  const response = await api.post(`/supplier`, supplier);
+  return response.data;
+}
 export async function getSupplier(id) {
-  const response = await api.get(`/Supplier/${id}`);
+  const response = await api.get(`/supplier/${id}`);
+  return response.data;
+}
+export async function getSupplierByName(name) {
+  const response = await api.get(`/supplier/${name}`);
+  return response.data;
+}
+export async function getSupplierByDocument(document) {
+  const response = await api.get(`/supplier/${document}`);
+  return response.data;
+}
+export async function getSupplierByRegisterTime(registerTime) {
+  const response = await api.get(`/supplier/${registerTime}`);
+  return response.data;
+}
+export async function getSupplierByCompany(company) {
+  const response = await api.get(`/supplier/${company}`);
   return response.data;
 }
 export async function getAllSuppliers() {
-  debugger;
   const response = await api.get(`/supplier`);
-  debugger;
+  return response.data;
+}
+export async function updateSupplier(supplier) {
+  const response = await api.put(`/supplier/${supplier.id}`, supplier);
+  return response.data;
+}
+export async function deleteSupplier(id) {
+  const response = await api.delete(`/supplier/${id}`);
   return response.data;
 }
