dd9a6ab4ff9dad1f64b0021ee77ac634f3567d6e
Larissa Tauana <laritauana@users.noreply.github.com>
2020-05-31 10:51:47 -0300

    Atualização migrations

---
+++ backend/Application/Migrations/20200531000629_Edição documento da empresa.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BludataTest.Migrations
+{
+    public partial class Ediçãodocumentodaempresa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Document",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CNPJ",
+                table: "Companies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CNPJ",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Document",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
