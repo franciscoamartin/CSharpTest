d962cf6ba07ad69b938ec2eb9c9dfc2bcf184f92
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-01 21:58:34 -0300

    Criação lista de telefones para a tabela

---
+++ frontend/src/components/TelephonesList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TelephonesList({ telephones }) {
+  return (
+    <div className="">
+      {telephones.map((tel) => (
+        <p>{te.nu}</p>
+      ))}
+    </div>
+  );
+}
