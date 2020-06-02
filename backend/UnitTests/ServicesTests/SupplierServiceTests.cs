ee9af3ccc8691529cdfc145b4d8d6bb2aee36b1d
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-02 11:43:37 -0300

    Adição teste unitário telefone e documentos

--- backend/UnitTests/ServicesTests/SupplierServiceTests.cs
+++ backend/UnitTests/ServicesTests/SupplierServiceTests.cs
@@ -29,8 +29,8 @@ namespace UnitTests.ServicesTests
         {
             var telephones = new List<Telephones>();
             telephones.Add(new Telephones("4733375582"));
-            telephones.Add(new Telephones("4733375582"));
-            telephones.Add(new Telephones("47991841670"));
+            telephones.Add(new Telephones("4733784158"));
+            telephones.Add(new Telephones("47992186559"));
             return telephones;
         }
 
@@ -53,7 +53,8 @@ namespace UnitTests.ServicesTests
              s.Document.ToString() == "086.263.709-03" &&
              s.RG == "623267" &&
              s.RegisterTime == DateTime.Now &&
-             s.BirthDate == new DateTime(2001, 12, 7)));
+             s.BirthDate == new DateTime(2001, 12, 7) &&
+             s.Telephone.ToString() == "4733375582"));
         }
 
         [Fact]
@@ -81,9 +82,27 @@ namespace UnitTests.ServicesTests
         }
 
         [Fact]
-        public void Should_return_all_suppliers()
+        public void Should_not_create_supplier_when_CPF_is_wrong()
         {
+            var supplier = GetSupplierExample();
+            supplier.Document = new Document("083.27.709-03", EDocumentType.CPF);
+            Assert.Throws<Exception>(() => _supplierService.Create(supplier));
+        }
 
+        [Fact]
+        public void Should_not_create_supplier_when_CNPJ_is_wrong()
+        {
+            var supplier = GetSupplierExample();
+            supplier.Document = new Document("18.321.410/001-42", EDocumentType.CNPJ);
+            Assert.Throws<Exception>(() => _supplierService.Create(supplier));
+        }
+
+        [Fact]
+        public void Should_not_create_supplier_when_telephone_is_wrong()
+        {
+            var supplier = GetSupplierExample();
+            supplier.Telephone = new List<Telephones>((3375886));
+            Assert.Throws<Exception>(() => _supplierService.Create(supplier));
         }
     }
 }
