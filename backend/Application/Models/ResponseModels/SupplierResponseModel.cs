f2ef09f9447462b87c56c91a60c8b1ce701ad37d
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-02 21:40:04 -0300

    Adicionando Data de cadastro e telefones no ResponseModel

--- backend/Application/Models/ResponseModels/SupplierResponseModel.cs
+++ backend/Application/Models/ResponseModels/SupplierResponseModel.cs
@@ -7,15 +7,21 @@ namespace BludataTest.ResponseModels
 {
     public class SupplierResponseModel : Supplier
     {
-        public SupplierResponseModel(string name, Company company, Guid companyId, Document document, string rg, DateTime registerTime, DateTime? birthDate, List<Telephones> telephone)
+        public SupplierResponseModel(Guid id, string name, Company company, Guid companyId, Document document,
+        string rg, DateTime registerTime, DateTime? birthDate, List<Telephones> telephone)
             : base(name, company, companyId, document, rg, registerTime, birthDate, telephone)
         {
+            Id = id;
             CpfCnpj = document.ToString();
             CompanyTradingName = company.TradingName;
+            FormattedRegisterTime = registerTime.ToString("dd/MM/yyyy HH:mm:ss");
+            FormattedTelephones = telephone.ConvertAll(tel => tel.Number);
         }
 
+        public List<string> FormattedTelephones { get; set; }
         public string CpfCnpj { get; set; }
         public string CompanyTradingName { get; set; }
+        public string FormattedRegisterTime { get; set; }
     }
 
 }
