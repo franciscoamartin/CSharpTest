3036bfacdf9c5125cf0723be317708b9d7c8de91
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-04 21:54:06 -0300

    Limpeza no código

--- backend/Application/Startup.cs
+++ backend/Application/Startup.cs
@@ -1,52 +1,52 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BludataTest.Models;
 using BludataTest.Repositories;
 using BludataTest.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace BludataTest {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace BludataTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<BludataTestDbContext> (options =>
-                options.UseSqlServer ("Server=localhost\\SQLEXPRESS;User Id=sa;Password=sa;Database=BludataTest;MultipleActiveResultSets=true"));
-            services.AddTransient<ICompanyRepository, CompanyRepository> ();
-            services.AddTransient<ICompanyService, CompanyService> ();
-            services.AddTransient<ISupplierRepository, SupplierRepository> ();
-            services.AddTransient<ISupplierService, SupplierService> ();
-            services.AddMvc ();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BludataTestDbContext>(options =>
+               options.UseSqlServer("Server=localhost\\SQLEXPRESS;User Id=sa;Password=sa;Database=BludataTest;MultipleActiveResultSets=true"));
+            services.AddTransient<ICompanyRepository, CompanyRepository>();
+            services.AddTransient<ICompanyService, CompanyService>();
+            services.AddTransient<ISupplierRepository, SupplierRepository>();
+            services.AddTransient<ISupplierService, SupplierService>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
-            app.UseCors (option => option.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ());
+            app.UseCors(option => option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
-            app.UseRouting ();
+            app.UseRouting();
 
-            app.UseAuthorization ();
+            app.UseAuthorization();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
         }
     }
