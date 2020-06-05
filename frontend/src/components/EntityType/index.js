386a3af3293da1358aec33749a35fe11ddb266d4
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-05 20:47:56 -0300

    Máscaras front-end

--- frontend/src/components/EntityType/index.js
+++ frontend/src/components/EntityType/index.js
@@ -1,39 +1,60 @@
 import InputMask from 'react-input-mask';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 export default function EntityType(props) {
+  function clearCPFInput() {
+    props.setRG('');
+    props.setDocumentNumber('');
+    props.setBirthDate('');
+  }
+
   return (
     <>
-      {props.documentType == 1 ? (
-        <InputMask
-          mask="99.999.999/9999-99"
-          value={props.documentNumber}
-          onChange={(e) => props.setDocumentNumber(e.target.value)}
-          placeholder="CNPJ"
-        ></InputMask>
-      ) : (
-        <>
+      <select
+        className="select-person"
+        onChange={(e) => {
+          clearCPFInput();
+          const stringValue = e.target.value;
+          props.setDocumentType(Number(stringValue));
+        }}
+      >
+        <option value={1}>CNPJ</option>
+        <option value={2}>CPF</option>
+      </select>
+      <div className="column entity-type-container">
+        {props.documentType == 1 ? (
           <InputMask
-            mask="999.999.999-99"
+            mask="99.999.999/9999-99"
             value={props.documentNumber}
             onChange={(e) => props.setDocumentNumber(e.target.value)}
-            placeholder="CPF"
+            placeholder="CNPJ"
           >
             {(inputProps) => <input {...inputProps} />}
           </InputMask>
-          <input
-            placeholder="RG"
-            value={props.rg}
-            onChange={(e) => props.setRG(e.target.value)}
-          />
-          <input
-            placeholder="Data de nascimento"
-            type="date"
-            value={props.birthDate}
-            onChange={(e) => props.setBirthDate(e.target.value)}
-          />
-        </>
-      )}
+        ) : (
+          <>
+            <InputMask
+              mask="999.999.999-99"
+              value={props.documentNumber}
+              onChange={(e) => props.setDocumentNumber(e.target.value)}
+              placeholder="CPF"
+            >
+              {(inputProps) => <input {...inputProps} />}
+            </InputMask>
+            <input
+              placeholder="RG"
+              value={props.rg}
+              onChange={(e) => props.setRG(e.target.value)}
+            />
+            <input
+              placeholder="Data de nascimento"
+              type="date"
+              value={props.birthDate}
+              onChange={(e) => props.setBirthDate(e.target.value)}
+            />
+          </>
+        )}
+      </div>
     </>
   );
 }
