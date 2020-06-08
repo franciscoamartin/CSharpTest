73b5bcd81b634b5d97c9f581ac09bedefe2ef52f
Larissa Tauana <laritauana@users.noreply.github.com>
2020-06-08 15:12:00 -0300

    Adições finais ResponseModels

--- backend/Application/Models/ResponseModels/SupplierResponseModel.cs
+++ backend/Application/Models/ResponseModels/SupplierResponseModel.cs
@@ -5,23 +5,29 @@ using BludataTest.ValueObject;
 
 namespace BludataTest.ResponseModels
 {
-    public class SupplierResponseModel : Supplier
+    public class SupplierResponseModel
     {
-        public SupplierResponseModel(Guid id, string name, Company company, Guid companyId, Document document,
-        string rg, DateTime registerTime, DateTime? birthDate, List<Telephones> telephone)
-            : base(name, company, companyId, document, rg, registerTime, birthDate, telephone)
+        public SupplierResponseModel(Guid id, string name, Document document, string companyTradingName,
+        string rg, DateTime registerTime, DateTime? birthDate, List<Telephone> telephone)
         {
             Id = id;
+            Name = name;
+            RG = rg;
             CpfCnpj = document.ToString();
-            CompanyTradingName = company.TradingName;
-            FormattedRegisterTime = registerTime.ToString("dd/MM/yyyy HH:mm:ss");
-            FormattedTelephones = telephone.ConvertAll(tel => tel.Number);
+            CompanyTradingName = companyTradingName;
+            BirthDate = birthDate?.ToString("dd/MM/yyyy");
+            RegisterTime = registerTime.ToString("dd/MM/yyyy HH:mm:ss");
+            Telephones = telephone.ConvertAll(tel => tel.Number);
         }
 
-        public List<string> FormattedTelephones { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public string RG { get; set; }
+        public List<string> Telephones { get; set; }
         public string CpfCnpj { get; set; }
         public string CompanyTradingName { get; set; }
-        public string FormattedRegisterTime { get; set; }
+        public string RegisterTime { get; set; }
     }
 
 }
